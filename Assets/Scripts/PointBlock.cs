a8db7f891cd14ed9e311c4e86dc2f8329c448311
Tony <toscarreks@gmail.com>
2019-04-12 15:57:54 -0700

    Added PointBlock component, Gold Block prefab

---
+++ Assets/Scripts/PointBlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// If this block falls below a certain point, it will be "collected"
+// depending on its assigned point value
+public class PointBlock : MonoBehaviour
+{
+    public int value = 10;
+    public float ground = -0.9f;
+    private Rigidbody rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Are we below the threshold
+        // We could also test for velocity
+        if(transform.position.y < ground && rb.velocity.magnitude < 0.1){
+            //initiate point animation
+            // then destroy the block
+            StartCoroutine(animateThenDestroy());
+        }
+    }
+
+    IEnumerator animateThenDestroy(){
+        //wait a second
+        yield return new WaitForSeconds(1.0f);
+        //in a for loop, do some sort of destroy animation
+
+        // Increment global "Points"
+
+        //Destroy the object
+        Debug.Log("You've earned " + value + " points!");
+        Destroy(gameObject);
+    }
+}
