8ca6d8a7dbc8f8c8f245dca607e7e6bfd00dcdd3
Andrew Nguyen <nguyenandrewq@yahoo.com>
2019-04-23 14:49:52 -0700

    implemented scoring ui

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // Keeps track of our points, will probably interface directly with the UI
 // Maybe it would be better to have this in a central gameManager...
 public class PointManager : MonoBehaviour
 {
+    public Text scoreText;
+
     public int points = 0;
 
     public void addPoints(int amt){
         points += amt;
         Debug.Log("Points added, total is now " + points);
     }
+    
+    void Update() {
+        scoreText.text = "Score: " + points;
+    }
+
 }
