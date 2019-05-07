808ac4446d885734dc5242395db87892dca4f8c4
Tony <toscarreks@gmail.com>
2019-05-07 15:14:19 -0700

    Added "Minecraft Controls"

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -6,17 +6,33 @@ public class Cannon : MonoBehaviour {
 
     public GameObject projectile;
     public float force_strength;
+    private bool is_charging = false;
+    public float charge_rate = 0.02f;
+    private float total_charge = 0f;
+    public float charge_max = 5f;
 
     void Update(){
         if(Input.GetKeyDown(KeyCode.Space)){
-            shoot();
+            is_charging = true;
         }
-        Debug.DrawLine(transform.position, transform.position + transform.forward.normalized * 10, Color.red, 2, false);
+        if(Input.GetKeyUp(KeyCode.Space)){
+            is_charging = false;
+            shoot(force_strength * total_charge);
+            total_charge = 0;
+        }
+
+        if(is_charging && total_charge < charge_max){
+            total_charge += charge_rate;
+            Debug.Log("Cannon Charge: " + total_charge);
+        }
+
+
+        //Debug.DrawLine(transform.position, transform.position + transform.forward.normalized * 10, Color.red, 2, false);
     }
 	
-    public void shoot(){
+    public void shoot(float power){
         GameObject new_p = Instantiate(projectile, transform.position, transform.rotation);
         Rigidbody rb = new_p.GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward.normalized * force_strength);
+        rb.AddForce(transform.forward.normalized * power);
     }
 }
---
+++ Assets/Scripts/MinecraftControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinecraftControls : MonoBehaviour
+{
+
+    CharacterController controller;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            // We don't need this
+            // yet
+
+            // if (Input.GetButton("Jump"))
+            // {
+            //     moveDirection.y = jumpSpeed;
+            // }
+
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
