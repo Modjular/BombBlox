b7433b2746179adc1626657d3e7e344463a0fe13
Tony <toscarreks@gmail.com>
2019-05-30 10:45:57 -0700

    added a primitive start menu

---
+++ Assets/Scripts/LoadNewSceneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewSceneScript : MonoBehaviour
+{
+    // Just a container for calling a new scene
+    // more of a workaround tbh
+
+    public void loadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
